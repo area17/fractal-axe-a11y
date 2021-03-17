43618fd59c33cf4134b615083abf1a2893be5c0c
Tim Brook <tim@area17.com>
2021-03-17 11:25:08 -0500

    Tidy up index.js

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const path = require('path');
 
-function setupAxeA11y(theme, opts) {
-  const config = opts || {};
-
-  // Test for the theme
+function setupAxeA11y(theme) {
   if ((typeof theme !== 'object') || (typeof theme.options !== 'function')) {
-      throw new Error('Please provide a valid Fractal theme as first argument');
+      throw new Error('Fractal Theme Config Missing');
   }
+
   theme.addLoadPath(path.resolve(__dirname, 'dist', 'views'));
-  theme.addStatic(path.resolve(__dirname, 'node_modules', 'axe-core'), 'axe-core');
   theme.addStatic(path.resolve(__dirname, 'dist', 'assets'), 'axe-a11y');
 
   const options = theme.options();
 
-  options.scripts = [].concat(...(options.scripts || ['default'])).concat([
-    `/axe-a11y/ui.js`
-  ]);
-
-  options.styles = [].concat(...(options.styles || ['default'])).concat(`/axe-a11y/ui.css`);
+  options.scripts = [...(options.scripts || ['default']), '/axe-a11y/ui.js'];
+  options.styles = [...(options.styles || ['default']), '/axe-a11y/ui.css'];
 
   if (options.panels.indexOf('a11y') < 0) {
       options.panels.push('a11y');
